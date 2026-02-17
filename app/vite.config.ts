bd2395b1b70c9df4777714a9915b3f03ea5e3ec6
Alexandre Muniz Emygdio <emygdio.alex@gmail.com>
2026-02-17 03:41:20 -0300

    ajuste

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/tailadmin-vuejs/' : '/',
+  base: '/', 
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
