5368470b1f132181960ddf9a5981be1569a9b5c6
Alexandre Muniz Emygdio <emygdio.alex@gmail.com>
2026-02-23 19:36:41 -0300

    ajuste env

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,8 +1,8 @@
 import app from './app.js'
 import { logger } from './plugins/logger'
 
-const HOST = process.env.HOST || 'app.bifrosthub.com'
-const PORT = Number(process.env.PORT) || 3000
+const HOST = process.env.HOST 
+const PORT = Number(process.env.PORT)
 
 app.listen({ port: PORT, host: HOST }).then(() => {
   logger.info(`🚀 Server running on http://${HOST}:${PORT}`)
