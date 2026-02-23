1ff028d18ad62e081d4d5658bf0616f53d5b232e
Alexandre Muniz Emygdio <emygdio.alex@gmail.com>
2026-02-23 19:45:52 -0300

    removido calendar antigo

--- app/backend/dist/server.js
+++ app/backend/dist/server.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_js_1 = __importDefault(require("./app.js"));
 const logger_1 = require("./plugins/logger");
-const HOST = process.env.HOST || 'app.bifrosthub.com';
-const PORT = Number(process.env.PORT) || 3000;
+const HOST = process.env.HOST;
+const PORT = Number(process.env.PORT);
 app_js_1.default.listen({ port: PORT, host: HOST }).then(() => {
     logger_1.logger.info(`🚀 Server running on http://${HOST}:${PORT}`);
 });
